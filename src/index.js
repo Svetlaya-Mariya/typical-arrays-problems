537c48e50475ffc48735f5da88db324dcaf2adba
Мария Светлая <merry76light@gmail.com>
2021-03-14 17:31:33 +0300

    feat: typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array === null || array.length == 0){
+    return 0;
+  }
+  else {
+    let minArr = array[0];
+    for (let i=1; i<array.length; i++){
+      if (array[i]< minArr){
+        minArr = array[i];
+      }
+    }
+    return minArr;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length){
+    let maxArr = array[0];
+    for (let i=1; i<array.length; i++){
+      if (array[i] > maxArr){
+        maxArr = array[i];
+      }
+    }
+    return maxArr;
+  }
+  else return 0;
 }
-
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let avgArr = 0;
+  if (array == undefined || array === null || array.length == 0){
+    return 0;
+  }
+
+  for (let i=0; i<array.length; i++){
+    sum+=array[i];
+  }
+  avgArr = sum/array.length;
+  return avgArr;
 }
